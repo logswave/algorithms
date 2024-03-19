2beee8b1466ea9c9be9432557e500887c40c980e
jhwmac <jkr2505@naver.com>
2024-03-19 22:18:41 +0900

    new: 20 (2024.03.19)

---
+++ 20/index.js
@@ -0,0 +1,41 @@
+// 문제 : 짝수의 합
+// 정수 n이 주어질 때, n이하의 짝수를 모두 더한 값을 return 하도록 solution 함수를 작성해주세요.
+
+// n	result
+// 10	30
+// 4	6
+
+
+// 쉬운 문제이나 어떻게 푸는 지가 관건이겠다.
+// reduce를 쓸것인가?
+// loop 를 돌리는 것이 정석일진데
+function solve(n) {
+    let arr = [...new Array(n)] // 아니 굳이?
+    return result
+}
+// n 개의 배열을 만들어서 음..
+// 뭔가 찝찝하다
+
+// 다른 쪽으로 머리를 굴려보자
+// 우선 1 ~ 10까지 합은 55
+// 1~ 100까지는 5050 이다.
+// 1 ~ 10 까지 짝수 합은 30
+// 나름의 규칙을 찾아보자
+function solve2(n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) count += i
+    return (count + n / 2) / 2
+}
+console.log(solve2(5))
+// 테스트 케이스 하나가 틀리다.
+// 홀수일때 소수점이나온다.
+
+function solve3(n) {
+    let count = 0;
+    let cut = n % 2 === 0 ? n : n - 1
+    for (let i = 1; i <= cut; i++) count += i
+    return (count + cut / 2) / 2
+}
+console.log(solve2(5))
+
+// 소수점에 대한 예외처리
--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 - 코딩 테스트 문제를 통해 문제 해결 능력을 기르고 알고리즘 지식을 쌓고자 합니다.
 
 ## 히스토리
+- **20.** [짝수의 합 (programmers)](https://github.com/logwaves/algorithms/blob/main/20/index.js) - [2024.03.19]
 - **19.** [숨어있는 숫자의 덧셈 2 (programmers)](https://github.com/logwaves/algorithms/blob/main/19/index.js) - [2024.03.17]
 - **18.** [합성수 찾기 (programmers)](https://github.com/logwaves/algorithms/blob/main/18/index.js) - [2024.03.17]
 - **17.** [안전구역 찾기 (programmers)](https://github.com/logwaves/algorithms/blob/main/17/index.js) - [2024.03.15]
