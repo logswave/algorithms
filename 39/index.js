ff9853b95852fa98e60559026321b6cfbda71548
jhwmac <jkr2505@naver.com>
2024-04-25 23:29:49 +0900

    new: 39 (2024.04.25)

--- 39/index.js
+++ 39/index.js
@@ -22,7 +22,13 @@
 // ["meosseugi", "1234"]	[["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]	"login"
 // ["programmer01", "15789"]	[["programmer02", "111111"], ["programmer00", "134"], ["programmer01", "1145"]]	"wrong pw"
 // ["rabbit04", "98761"]	[["jaja11", "98761"], ["krong0313", "29440"], ["rabbit00", "111333"]]	"fail"
-function solve() {
-    let result;
-    return result
+function solve(id_pw, db) {
+    for (let i = 0; i < db.length; i++) {
+        if (db[i][0] == id_pw[0]) {
+            if (db[i][1] == id_pw[1]) return "login"
+            return "wrong pw"
+        }
+    }
+    return "fail"
 }
+console.log(solve(["rabbit04", "98761"], [["jaja11", "98761"], ["krong0313", "29440"], ["rabbit00", "111333"]]))
